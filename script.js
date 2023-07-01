f9ae3c0e2e97a4ea7011da67eafc891a9bb61aef
Edmuna <ereda.hr@gmail.com>
2023-07-01 20:44:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const x = document.querySelector(".x-icon");
+const bars = document.querySelector(".bar-icon");
+const menu = document.querySelector(".navbar");
+const blabla = document.querySelectorAll(".bar-icon.active");
+const overlay = document.querySelector(".overlay");
+
+x.addEventListener("click", () => {
+  x.classList.add("closed");
+  bars.classList.remove("closed");
+  menu.classList.add("closed");
+  document.body.style.overflowY = "hidden";
+  overlay.classList.remove("full");
+});
+
+bars.addEventListener("click", () => {
+  x.classList.remove("closed");
+  bars.classList.add("closed");
+  menu.classList.remove("closed");
+  document.body.style.overflowY = "auto";
+  overlay.classList.add("full");
+});
